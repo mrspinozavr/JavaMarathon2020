eb30c77f3e9fdfc7f0000c43b6540413ea58076c
Alexandr Osiptsov <solamer@yandex.ru>
2020-12-26 21:16:28 +0300

    Add Day3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double x, y;
+        while (true) {
+            x = scanner.nextDouble();
+            y = scanner.nextDouble();
+            if (y == 0) {
+                break;
+            } else {
+                System.out.println(x / y);
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double x, y;
+        for (int i = 0; i < 5; i++) {
+            x = scanner.nextDouble();
+            y = scanner.nextDouble();
+            if (y == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(x / y);
+        }
     }
 }
