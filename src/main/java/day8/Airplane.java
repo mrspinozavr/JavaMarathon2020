fa9973e362a7c12a72168710f529c2927af4688e
Alexandr Osiptsov <solamer@yandex.ru>
2021-01-03 13:47:32 +0300

    Add Day8 solution

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,54 @@
+package day8;
+
+public class Airplane {
+
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.length = length;
+        this.weight = weight;
+        this.year = year;
+        this.fuel = 0;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void fillUp(int n) {
+        this.fuel += n;
+    }
+
+    public void info() {
+        System.out.printf("Изготовитель: %s, год выпуска: %d, длина: %d, вес: %d, количество топлива в баке: %d\n", producer, year, length, weight, fuel);
+    }
+
+    public String toString() {
+        return "Изготовитель: " + producer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel;
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,20 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        StringBuilder sb = new StringBuilder();
+        String result = "";
+        long start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        long finish = System.currentTimeMillis();
+        System.out.println(sb);
+        System.out.printf("Работа цикла с StringBuilder заняла %d мс\n", finish - start);
+        start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            result += i + " ";
+        }
+        finish = System.currentTimeMillis();
+        System.out.printf("Работа цикла с String заняла %d мс\n", finish - start);
     }
 }
