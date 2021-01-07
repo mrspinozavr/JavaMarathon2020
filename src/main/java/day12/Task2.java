92f37e8e6c588ac5a286663d0f1a358234caf1c0
Alexandr Osiptsov <solamer@yandex.ru>
2021-01-07 23:59:19 +0300

    Add Day12 fix

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -11,11 +11,11 @@ public class Task2 {
 
     public static List<Integer> initNewArrayEvenNums() {
         List<Integer> nums = new ArrayList<>();
-        for (int i = 0; i <= 30; i += 2) {
+        for (int i = 0; i <= 350; i += 2) {
             nums.add(i);
-        }
-        for (int i = 300; i <= 350; i += 2) {
-            nums.add(i);
+            if(i == 30) {
+                i = 298;
+            }
         }
         return nums;
     }
